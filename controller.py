4f9fa18879bc3a0f0c7dd1dab0d0055ab0753672
Ianm22 <infinitomix@gmail.com>
2021-05-29 21:34:07 +0200

    Some improvements

--- controller.py
+++ controller.py
@@ -1,36 +1,52 @@
 from model import *
+from pathlib import Path
+import re
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-arrayApps = [None,None,None,None]
-dir_scripts = os.path.dirname("/home/paul/.config/multi-win/") 
-dir_quick_access = os.path.dirname("/home/paul/.local/share/applications/")
+# Variables
+#arrayApps = [None,None,None,None]
+selected_apps = {
+    'app1': None,
+    'app2': None,
+    'app3': None,
+    'app4': None
+}
 
+home = str(Path.home())
+
+config_dir_scripts = "/.config/multi-win/"
+dir_scripts = os.path.dirname(home + re.sub("/home/[A-Za-z]/", "",config_dir_scripts))
+
+config_dir_quick_access =  "/.local/share/applications/"
+dir_quick_access = os.path.dirname(home + re.sub("/home/[A-Za-z]/", "",config_dir_quick_access))
+
+# Class
 class Handler:
 
     def __init__(self, builder_in):
         self.builder = builder_in
 
     def getAppChooserDialog(self, *arg, label=str):
-        print(arrayApps)
+        print(selected_apps)
         
         def on_response(dialog, response):
             if response == Gtk.ResponseType.OK:
                 app_info = dialog.get_app_info()
                 
                 if label == 'Select App 1':
-                    arrayApps[0] = app_info.get_commandline()
+                    selected_apps['app1'] = app_info.get_commandline()
                     self.builder.get_object('lbl_app_1').set_label(app_info.get_display_name())
                 elif label == 'Select App 2':
                     self.builder.get_object('lbl_app_2').set_label(app_info.get_display_name())
-                    arrayApps[1] = app_info.get_commandline()
+                    selected_apps['app2'] = app_info.get_commandline()
                 elif label == 'Select App 3':
                     self.builder.get_object('lbl_app_3').set_label(app_info.get_display_name())
-                    arrayApps[2] = app_info.get_commandline()
+                    selected_apps['app3'] = app_info.get_commandline()
                 elif label == 'Select App 4':
                     self.builder.get_object('lbl_app_4').set_label(app_info.get_display_name())
-                    arrayApps[3] = app_info.get_commandline()
+                    selected_apps['app4'] = app_info.get_commandline()
                 
             dialog.destroy()
 
@@ -52,20 +68,18 @@ class Handler:
         self.builder.get_object('app_box_4').hide()
         self.builder.get_object('lbl_app_3').set_label('App 3')
         self.builder.get_object('lbl_app_4').set_label('App 4')
-        arrayApps[2] = None
-        arrayApps[3] = None
-
+        selected_apps['app3'] = None
+        selected_apps['app4'] = None
 
     def select_3(self, radiobutton):
         self.builder.get_object('app_box_3').show()
         self.builder.get_object('app_box_4').hide()
         self.builder.get_object('lbl_app_4').set_label('App 4')
-        arrayApps[3] = None 
+        selected_apps['app4'] = None
 
     def select_4(self, radiobutton):
         self.builder.get_object('app_box_3').show() 
         self.builder.get_object('app_box_4').show()
-    
     
 
     def app_btn_1(self, widget):
@@ -84,9 +98,18 @@ class Handler:
         print(widget.get_label())
         self.getAppChooserDialog(label=widget.get_label())
 
+
     def createApp_btn(self, button):
         new_app_name = self.builder.get_object('lbl_newAppName').get_text()
         
-        create_script(new_app_name, arrayApps, dir_scripts)
-        create_quick_access(new_app_name, dir_quick_access, dir_scripts, arrayApps)
+        output_script = create_script(new_app_name, selected_apps, dir_scripts)
+        output_QA = create_quick_access(new_app_name, dir_quick_access, dir_scripts)
+
+        if output_script and output_QA:
+            self.builder.get_object('lbl_output').set_text('All ok!')
+        else:
+            self.builder.get_object('lbl_output').set_text('Something went wrong!')
+
+        
+
 
--- model.py
+++ model.py
@@ -3,57 +3,72 @@ import re
 
 # ------------ Functions ------------------
 # Function what creates the .desktop for execute
-def create_script(name_quick_access, arrayApps, dir_scripts):
+def create_script(name_quick_access, selected_apps, dir_scripts):
 
     try:
-        os.makedirs(dir_scripts, 0o500)
-    except FileExistsError:
-        print("The folder has already been created")
+        try:
+            os.makedirs(dir_scripts, 0o500)
+        except FileExistsError:
+            print("The folder has already been created")
 
-    file = open("{}/{}.sh".format(dir_scripts, name_quick_access),"w+")
-    file.write("#!/bin/bash\n")
+        file = open("{}/{}.sh".format(dir_scripts, name_quick_access),"w+")
+        file.write("#!/bin/bash\n")
 
-    if arrayApps[0] is not None:
-        file.write("{} &\n".format(re.sub(" %[A-Za-z]","",arrayApps[0])))
+        if selected_apps['app1'] is not None:
+            file.write("{} &\n".format(re.sub(" %[A-Za-z]", "", selected_apps['app1'])))
 
-    if arrayApps[1] is not None:
-        file.write("{} &\n".format(re.sub(" %[A-Za-z]","",arrayApps[1])))
+        if selected_apps['app2'] is not None:
+            file.write("{} &\n".format(re.sub(" %[A-Za-z]", "", selected_apps['app2'])))
 
-    if arrayApps[2] is not None:
-        file.write("{} &\n".format(re.sub(" %[A-Za-z]","",arrayApps[2])))
+        if selected_apps['app3'] is not None:
+            file.write("{} &\n".format(re.sub(" %[A-Za-z]", "", selected_apps['app3'])))
 
-    if arrayApps[3] is not None:
-        file.write("{} &\n".format(re.sub(" %[A-Za-z]","",arrayApps[3])))
+        if selected_apps['app4'] is not None:
+            file.write("{} &\n".format(re.sub(" %[A-Za-z]", "", selected_apps['app4'])))
 
-    file.close
+        file.close
 
-    print('Script created')
-    os.chmod("{}/{}.sh".format(dir_scripts, name_quick_access), 0o744)
+        print('Script created')
+        os.chmod("{}/{}.sh".format(dir_scripts, name_quick_access), 0o744)
+        output = True
+    
+    except:
+        output = False
+
+    return output
     
 
 
 # Function what creates the .desktop for execute
-def create_quick_access(name_quick_access, dir_quick_access, dir_scripts, arrayApps):
+def create_quick_access(name_quick_access, dir_quick_access, dir_scripts):
 
     try:
-        os.makedirs(dir_quick_access, 0o500)
-    except FileExistsError:
-        print("The folder has already been created")
+        try:
+            os.makedirs(dir_quick_access, 0o500)
+        except FileExistsError:
+            print("The folder has already been created")
 
 
-    file = open("{}/{}.desktop".format(dir_quick_access, name_quick_access),"w+")
+        file = open("{}/{}.desktop".format(dir_quick_access, name_quick_access),"w+")
 
 
-    file.write("""[Desktop Entry]
-Encoding=UTF-8
-Version=1.0
-Type=Application
-Terminal=false
-Exec={}/'{}.sh'
-Name={}
-Comment=Custom app;
-""".format(dir_scripts, name_quick_access, name_quick_access))
+        file.write(
+        """[Desktop Entry]
+        Encoding=UTF-8
+        Version=1.0
+        Type=Application
+        Terminal=false
+        Exec={}/'{}.sh'
+        Name={}
+        Comment=Custom app;
+        """.format(dir_scripts, name_quick_access, name_quick_access))
 
-    file.close
-    print('Quick access created')
-    os.chmod("{}/{}.desktop".format(dir_quick_access, name_quick_access), 0o744)
+        file.close
+        print('Quick access created')
+        os.chmod("{}/{}.desktop".format(dir_quick_access, name_quick_access), 0o744)
+        output = True
+
+    except:
+        output = False
+
+    return output
